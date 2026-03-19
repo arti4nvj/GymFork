f98b64e3c27dba3a0e86177ca8e8a98c9c08074c
Arti <artij@nvidia.com>
2026-03-18 17:59:16 -0700

    license added
    Signed-off-by: Arti <artij@nvidia.com>
    Signed-off-by: Arti Jain <artij@nvidia.com>

--- resources_servers/cvdp/scripts/cvdp_pass_at_k_report.py
+++ resources_servers/cvdp/scripts/cvdp_pass_at_k_report.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 """
 Convert a NeMo-Gym pass@k rollout JSONL into the full CVDP report layout:
 
